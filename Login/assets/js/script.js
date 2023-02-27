ea0f41935e7e49eb29666e43f1423d1bd4884b36
anarmh <anarmh@code.edu.az>
2023-02-27 02:05:01 -0800

    update

--- Login/assets/js/script.js
+++ Login/assets/js/script.js
@@ -3,32 +3,44 @@
 let emailInput=document.getElementById("email");
 let passwordInput=document.getElementById("password");
 let loginButton=document.getElementById("login-btn");
+let myForm = document.getElementById("myForm");
+let errors=document.getElementById("errors");
+myForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    login();
+})
 
-loginButton.addEventListener("click", login);
-emailInput.addEventListener("click",function(e){
-    if(e.key==="Enter"){
+// loginButton.addEventListener("click", login);
+// emailInput.addEventListener("click",function(e){
+//     if(e.key==="Enter"){
        
-        login()
-    }
-})
+//         login()
+//     }
+// })
 
-passwordInput.addEventListener("click",function(e){
-    if(e.key==="Enter"){
+// passwordInput.addEventListener("click",function(e){
+//     if(e.key==="Enter"){
         
-        login()
-    }
-})
+//         login()
+//     }
+// })
+document.onkeydown=function(e){
+    if(e.keyCode==13)
+    login();
+}
+
 
 
 function login(){
 
     let email=emailInput.value.trim();
     let password=passwordInput.value.trim();
-
-    if(email===""|| password===""){
+    if(email==="" || password===""){
         let error=document.createElement("p");
         error.innerText="Bosh buraxmayin";
         error.style.color="red"
+        errors.append(error)
+
     }
     else{
         alert("Login olunduz")
